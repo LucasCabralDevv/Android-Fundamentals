360d1ca123fa404bceedc558224b7ccf768534ac
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-08-18 09:07:28 -0300

    Sucesso ao recuperar contatos do dispositivo.

--- app/src/main/java/com/lucascabral/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/lucascabral/androidfundamentals/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.lucascabral.androidfundamentals
 
+import android.Manifest
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.lucascabral.androidfundamentals.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonContacts.setOnClickListener {
+            getPhoneContacts()
+        }
+    }
+
+    private fun getPhoneContacts() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), 0)
+        }
+
+        val contentResolver: ContentResolver = contentResolver
+        val uri: Uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
+        Log.i("CONTACT_PROVIDER_DEMO", "TOTAL of Contacts: ${cursor?.count}")
+        cursor?.let {
+            while (cursor.moveToNext()) {
+                val contactName: String =
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                val contactNumber =
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                Log.i("CONTACT_PROVIDER_DEMO", "Contact Name: $contactName, Number: $contactNumber")
+            }
+        }
     }
 }
